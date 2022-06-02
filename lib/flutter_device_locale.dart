8a7744c3a07f89057eff760d9ac491aff60a5b34
Rodrigo Miyashiro <rodrigo.miyashiro@yahoo.com.br>
2022-06-02 15:30:37 -0300

    fix List<String>

--- lib/flutter_device_locale.dart
+++ lib/flutter_device_locale.dart
@@ -1,38 +1,33 @@
-import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter_device_locale/src/platform.dart';
 
-class DeviceLocale 
-{
-    static Future<Locale> getCurrentLocale() async 
-    {
-        final deviceLocales = await getPreferredLocales();
+class DeviceLocale {
+  static Future<Locale> getCurrentLocale() async {
+    final deviceLocales = await getPreferredLocales();
 
-        return deviceLocales.first;
-    }
+    return deviceLocales.first;
+  }
 
-    static Future<List<Locale>> getPreferredLocales() async 
-    {
-        final List deviceLocales =
+  static Future<List<Locale>> getPreferredLocales() async {
+    final List<String> deviceLocales =
         await FlutterDeviceLocalePlatform.instance.deviceLocales();
 
-        return deviceLocales.map((x) => _localeFromString(x)).toList();
+    return deviceLocales.map((locale) => _localeFromString(locale)).toList();
+  }
+
+  static Locale _localeFromString(String code) {
+    var separator = code.contains('_')
+        ? '_'
+        : code.contains('-')
+            ? '-'
+            : null;
+
+    if (separator != null) {
+      var parts = code.split(RegExp(separator));
+      return Locale(parts[0], parts[1]);
     }
 
-    static Locale _localeFromString(String code) 
-    {
-        var separator = code.contains('_') ? '_' : code.contains('-') ? '-' : null;
-
-        if (separator != null) 
-        {
-            var parts = code.split(RegExp(separator));
-
-            return Locale(parts[0], parts[1]);
-        } 
-        else 
-        {
-            return Locale(code);
-        }
-    }
+    return Locale(code);
+  }
 }
